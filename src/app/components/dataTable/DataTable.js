bf0984d89567c7896239a2ab0117e1152f7ef25c
Jay Wood <jwoodcodes@gmail.com>
2025-02-14 21:23:38 -0600

    starting to build front end

---
+++ src/app/components/dataTable/DataTable.js
@@ -0,0 +1,9 @@
+import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+
+export default function DataTable() {
+    return (
+        <div></div>
+    )
+}
