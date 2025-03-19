20c2b0bb85fa3ce8ef5a38d79a3be35356390aad
J Wood <73401121+DFK333@users.noreply.github.com>
2025-03-19 16:53:47 -0500

    added scatterplot and started adjustable spider chart

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,31 @@
-
-
 import React from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 import MainNav from "./components/mainNav/MainNav";
-import DataFetcher from './components/dataTable/DataFetcher';
-
+import DataFetcher from "./components/dataTable/DataFetcher";
 
 export default function Home() {
-
-  
-  
-
   return (
     <div className={styles.page}>
       <MainNav />
       <main className={styles.main}>
-      <Image
+        <Image
           src="/star-bar.jpg"
           alt="starry night"
           className={styles.starBar}
-          width={2000}
+          width={3000}
           height={150}
           priority
-        /> 
+        />
         <div className={styles.titleWrapper}>
           <h1 className={styles.title}>Prospect Data By Class</h1>
-          <h2 className={styles.secondTitle}>click players name to view full prospect profile</h2>
+          <h2 className={styles.secondTitle}>
+            click players name to view full prospect profile
+          </h2>
         </div>
-        
-        
-       
+
         <DataFetcher />
       </main>
-      
     </div>
   );
 }
