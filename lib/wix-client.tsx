7529c17775d674c325d907cabe3fed7d1357382e
Jay Wood <jwoodcodes@gmail.com>
2025-04-04 22:09:51 -0500

    d

--- lib/wix-client.tsx
+++ lib/wix-client.tsx
@@ -4,8 +4,8 @@ import { createClient, OAuthStrategy } from "@wix/sdk"
 import { members } from "@wix/members"
 import Cookies from "js-cookie"
 
-let refreshToken = JSON.parse(Cookies.get('refreshToken')!)
-let accessToken = JSON.parse(Cookies.get('accessToken')!)
+let refreshToken = Cookies.get('refreshToken') ? JSON.parse(Cookies.get('refreshToken')) : null;
+let accessToken = Cookies.get('accessToken') ? JSON.parse(Cookies.get('accessToken')) : null;
 
 export const wixClient = createClient({
     
