2c6ce809cc4408a7da56c646b9c5176c899cbadd
Jay Wood <jwoodcodes@gmail.com>
2025-04-05 21:21:06 -0500

    d2

--- eslintrc.js
+++ eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:next/recommended',
         'next/core-web-vitals',
     ],
     rules: {
