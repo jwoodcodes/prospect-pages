30c2ba0e96938b5d87d5ae3ad24834f25085b20a
Jay Wood <jwoodcodes@gmail.com>
2025-04-07 16:35:11 -0500

    r1

--- eslintrc.js
+++ eslintrc.js
@@ -1,16 +1,6 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:next/recommended',
-        'next/core-web-vitals',
-    ],
-    rules: {
-      // your rules here
-    }
+  extends: ['next/core-web-vitals'],
+  rules: {
+    // your rules here
   }
+}
