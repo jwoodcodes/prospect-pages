442de1bba18e9d5abf32ce4a8ec3be94651a06a3
Jay Wood <jwoodcodes@gmail.com>
2025-04-29 18:06:27 -0500

    fixed draft row layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,6 +32,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>FFA Rookie Guide</title>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${hemiHead.className} ${playRegular.className}`}>
         
           {children}
